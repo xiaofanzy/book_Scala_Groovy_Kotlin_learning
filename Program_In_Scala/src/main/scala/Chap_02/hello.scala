74552b124c39fd8a20781aced9946bfac3bfa93f
zhangxiaofan <53987619+xiaofanzy@users.noreply.github.com>
2024-01-04 09:06:43 +0800

    第三章代码提交

--- Program_In_Scala/src/main/scala/Chap_02/hello.scala
+++ Program_In_Scala/src/main/scala/Chap_02/hello.scala
@@ -1,3 +1,18 @@
+/*
 @main def m(args: String*) =
-  args.foreach(arg => println(arg))
-11
+  args.foreach(arg => println(arg))*/
+
+/*@main def m(args : String*) =
+  args.foreach((arg: String) => println(arg))*/
+
+/*@main def m(args : String*) =
+  args.foreach(println)*/
+
+/**
+ * 神奇的for 循环操作；真NB
+ * @param args
+ */
+/*@main def m(args :String*) =
+  for arg <- args do
+    println(arg)*/
+
